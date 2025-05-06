f9e0526aeab8bc73856625aed3ba1f47ee619f71
lokesh-patil57 <luckypatil577R@gmail.com>
2025-05-06 18:05:21 +0530

    conpleted the listing

--- models/listing.js
+++ models/listing.js
@@ -11,6 +11,7 @@ const listingSchema = new Schema({
   },
   image: {
     type: String,
+    default : "https://media.assettype.com/freepressjournal/2023-02/51ded765-ba46-4902-801b-5f7373f340d6/fdvcx.png?width=1200",
     set : (v)=> v===""? "https://media.assettype.com/freepressjournal/2023-02/51ded765-ba46-4902-801b-5f7373f340d6/fdvcx.png?width=1200" : v ,
   },
   price: {
