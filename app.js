1396aaacf7cb3e39754d4aa0e2d239c614517216
lokesh-patil57 <luckypatil577R@gmail.com>
2025-05-06 18:56:00 +0530

    Index Route completed

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const MONGO_URL = "mongodb://127.0.0.1:27017/StayNest"
-const Listing = require("../StayNest/models/listing.js")
+const MONGO_URL = "mongodb://127.0.0.1:27017/StayNest";
+const Listing = require("../StayNest/models/listing.js");
+const path = require("path")
+
+app.set("view engine" , "ejs")
+app.set("views" , path.join(__dirname,"views"))
 
 main()
   .then(() => {
@@ -14,20 +18,25 @@ async function main() {
   await mongoose.connect(MONGO_URL);
 }
 
+//Index Route
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings });
+});
 
-app.get("/testlisting" , async (req , res) =>{
-    let sampleListing = new Listing({
-        title : "New Villa !!",
-        description: "near Goa Beach !!",
-        price : 1200,
-        location : "Calangute , Goa",
-        country : "India"
-    }) 
-    await sampleListing.save()
-    console.log(`Sample was Saved`);
-    res.send("Successful testing")
-    
-})
+// app.get("/testlisting" , async (req , res) =>{
+//     let sampleListing = new Listing({
+//         title : "New Villa !!",
+//         description: "near Goa Beach !!",
+//         price : 1200,
+//         location : "Calangute , Goa",
+//         country : "India"
+//     })
+//     await sampleListing.save()
+//     console.log(`Sample was Saved`);
+//     res.send("Successful testing")
+
+// })
 
 app.get("/", (req, res) => {
   res.send("I am Root");
