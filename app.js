5a469ec8824f457faeb9e5b53971a17a4a75fe25
lokesh-patil57 <luckypatil577R@gmail.com>
2025-05-08 23:49:01 +0530

    completed update and edit route

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const app = express();
 const mongoose = require("mongoose");
 const MONGO_URL = "mongodb://127.0.0.1:27017/StayNest";
 const Listing = require("../StayNest/models/listing.js");
-const path = require("path")
+const path = require("path");
+const methodOverride = require("method-override");
 
 app.use(express.urlencoded({ extended: true }));
-app.set("view engine" , "ejs")
-app.set("views" , path.join(__dirname,"views"))
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(methodOverride("_method"));
 
 main()
   .then(() => {
@@ -26,34 +28,38 @@ app.get("/listings", async (req, res) => {
 });
 
 //Create new route
-app.get("/listings/new",  (req, res )=>{
-  res.render("listings/new.ejs")
-})
-
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
+});
 
 
 //Show Route
-app.get("/listing/:id" , async (req , res)=>{
- let {id} = req.params
- const listing = await Listing.findById(id)
- res.render("listings/show.ejs" , {listing})
-})
+app.get("/listing/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
+});
 
 //Edit Route
-app.get("/listings/:id/edit", async (req,res)=>{
-  let {id} = req.params
-  let listing = await Listing.findById(id)
-  res.render("listings/edit.ejs" , {listing})
-})
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  let listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
+});
+
+//Upadate Route
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, {...req.body.listing });
+  res.redirect(`/listing/${id}`)
+});
 
 //Add new Listing Route
-app.post("/listings" , async (req , res )=>{
-  const newListing = new Listing(req.body.listing)
-  await newListing.save()
-  res.redirect("/listings")
-  
-
-})
+app.post("/listings", async (req, res) => {
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect(`/listings/${id}`);
+});
 
 // app.get("/testlisting" , async (req , res) =>{
 //     let sampleListing = new Listing({
