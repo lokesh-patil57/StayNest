32c63d98ca7c272834b7311a40bce55e9b2b9231
Lucky Patil <luckypatil577R@gmail.com>
2025-09-05 23:18:34 +0530

    review added to listings

--- models/listing.js
+++ models/listing.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const review = require("./review");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
@@ -23,6 +24,12 @@ const listingSchema = new Schema({
   country: {
     type: String,
   },
+  reviews:[
+    {
+      type: Schema.Types.ObjectId,
+      ref:"Review",
+    },
+  ]
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
