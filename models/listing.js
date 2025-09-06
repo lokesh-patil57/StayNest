186741579de3398c1857b94edcde526d1745d790
Lucky Patil <luckypatil577R@gmail.com>
2025-09-06 12:48:22 +0530

    added reviews for listing

--- models/listing.js
+++ models/listing.js
@@ -1,35 +1,37 @@
 const mongoose = require("mongoose");
-const review = require("./review");
+const Review = require("./review");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
   title: {
     type: String,
-    required : true
+    required: true,
   },
-  description: {
-    type: String,
-  },
+  description: String,
   image: {
     type: String,
-    default : "https://media.assettype.com/freepressjournal/2023-02/51ded765-ba46-4902-801b-5f7373f340d6/fdvcx.png?width=1200",
-    set : (v)=> v===""? "https://media.assettype.com/freepressjournal/2023-02/51ded765-ba46-4902-801b-5f7373f340d6/fdvcx.png?width=1200" : v ,
+    default:
+      "https://media.assettype.com/freepressjournal/2023-02/51ded765-ba46-4902-801b-5f7373f340d6/fdvcx.png?width=1200",
+    set: (v) =>
+      v === ""
+        ? "https://media.assettype.com/freepressjournal/2023-02/51ded765-ba46-4902-801b-5f7373f340d6/fdvcx.png?width=1200"
+        : v,
   },
-  price: {
-    type: Number,
-  },
-  location: {
-    type: String,
-  },
-  country: {
-    type: String,
-  },
-  reviews:[
+  price: Number,
+  location: String,
+  country: String,
+  reviews: [
     {
       type: Schema.Types.ObjectId,
-      ref:"Review",
+      ref: "Review",
     },
-  ]
+  ],
+});
+
+listingSchema.post("findOneAndDelete", async (listing) => {
+  if (listing) {
+    await Review.deleteMany({ _id: { $in: listing.reviews } });
+  }
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
