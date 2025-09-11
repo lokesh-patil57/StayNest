93403796a54e26576b015723ec7140bbdc821d25
Lucky Patil <luckypatil577R@gmail.com>
2025-09-11 10:16:49 +0530

    Login page for useer

--- routes/user.js
+++ routes/user.js
@@ -2,23 +2,43 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
+const passport = require("passport");
 
 router.get("/signup", (req, res) => {
   res.render("users/signup.ejs");
 });
 
-router.post("/signup", wrapAsync(async (req, res) => {
-  try {
-    let { username, password, email } = req.body;
-  const newUser = new User({ email, username });
-  const registeredUser = await User.register(newUser, password);
-    console.log(registeredUser)
-    req.flash("success","Welcome to StayNest !")
+router.post(
+  "/signup",
+  wrapAsync(async (req, res) => {
+    try {
+      let { username, password, email } = req.body;
+      const newUser = new User({ email, username });
+      const registeredUser = await User.register(newUser, password);
+      console.log(registeredUser);
+      req.flash("success", "Welcome to StayNest !");
+      res.redirect("/listings");
+    } catch (e) {
+      req.flash("error", e.message);
+      res.redirect("/signup");
+    }
+  })
+);
+
+router.get("/login", (req, res) => {
+  res.render("users/login.ejs");
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+  async (req, res) => {
+    req.flash("success","Welcome to StayNest you logged in!")
     res.redirect("/listings")
-  } catch (e) {
-    req.flash("error", e.message)
-    res.redirect("/signup")
   }
-}));
+);
 
 module.exports = router;
