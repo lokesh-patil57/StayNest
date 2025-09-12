908e144444b38deffe65ea03cbea1ddade597397
Lucky Patil <luckypatil577R@gmail.com>
2025-09-12 15:21:01 +0530

    post login page

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
 const passport = require("passport");
+const { saveRedirectUrl } = require("../middleware.js");
 
 router.get("/signup", (req, res) => {
   res.render("users/signup.ejs");
@@ -36,13 +37,14 @@ router.get("/login", (req, res) => {
 
 router.post(
   "/login",
+  saveRedirectUrl,
   passport.authenticate("local", {
     failureRedirect: "/login",
     failureFlash: true,
   }),
   async (req, res) => {
     req.flash("success", "Welcome to StayNest you logged in!");
-    res.redirect("/listings");
+    res.redirect(res.locals.redirectUrl || "/listings"); // <-- use res.locals
   }
 );
 
