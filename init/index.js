7e514d814247006ce934dce3b7601b2687e019a6
Lucky Patil <luckypatil577R@gmail.com>
2025-09-12 16:08:43 +0530

    Saving the listing owner

--- init/index.js
+++ init/index.js
@@ -3,7 +3,6 @@ const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 const MONGO_URL = "mongodb://127.0.0.1:27017/StayNest";
 
-
 main()
   .then(() => {
     console.log(`connected to mongoDB`);
@@ -16,6 +15,10 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "68c24b58ac04d8d8bb5880f8",
+  }));
   await Listing.insertMany(initData.data);
   console.log("Data was Initialized");
 };
